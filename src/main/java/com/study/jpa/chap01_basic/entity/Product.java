acab2a3801048af21d29169adad7e70f0a856a6f
lee1234 <whdtjs152413@gmail.com>
2023-11-20 13:07:24 +0900

    1. 엔터티 클래스 생성과 기본 CRUD

---
+++ src/main/java/com/study/jpa/chap01_basic/entity/Product.java
@@ -0,0 +1,42 @@
+package com.study.jpa.chap01_basic.entity;
+
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter @Setter
+@ToString @EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "tbl_product")
+public class Product {
+
+    @Column(name = "prod_id")
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "prod_name", nullable = false, unique = true, length = 30)
+    private String name;
+
+    private int price;
+
+    @Enumerated(EnumType.STRING)
+    private Category category;
+
+    @CreationTimestamp //default sysdate
+    @Column(updatable = false)
+    private LocalDateTime createDate;
+
+    @UpdateTimestamp
+    private LocalDateTime updateDate;
+
+    public enum Category {
+        FOOD, FASHION, ELECTRONIC
+    }
+}
